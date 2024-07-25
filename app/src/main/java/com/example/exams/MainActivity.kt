49ea1a5ef0862a5aff02fa57d7d3157b4bbe9c0e
Luka <luka.b.glavonjic@gmail.com>
2024-07-25 19:34:20 +0200

    first commit

---
+++ app/src/main/java/com/example/exams/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.exams
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+}
